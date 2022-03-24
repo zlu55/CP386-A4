8312e729dd95d270ce707f9ffabd2388d07bcd8a
adamgerrish <98437779+adamgerrish@users.noreply.github.com>
2022-03-24 11:36:33 -0400

    Updates to comment header

--- 191739390_190454440_a04_q2.c
+++ 191739390_190454440_a04_q2.c
@@ -8,5 +8,12 @@ Adam Gerrish
 190454440
 gerr4440@mylaurier.ca
 
-This is a test
+CP386 Assignment 4 - Question 2
+Date: 2022-03-24
+Description: Uses Best-Fit algorithm for contiguous memory allocation. 
+This project will involve managing a contiguous region of memory of 
+size MAX where addresses may range from 0 ... MAX - 1.
+
 */
+
+
